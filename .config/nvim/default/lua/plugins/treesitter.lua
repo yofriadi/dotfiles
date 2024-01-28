d783022b8db053a54f48fa2b30d4075088bf5435
Yofriadi Yahya <yofriadi.yahya@outlook.com>
2024-01-28 20:48:45 +0700

    feat(neovim): add just to nvim-treesitter

--- .config/nvim/default/lua/plugins/treesitter.lua
+++ .config/nvim/default/lua/plugins/treesitter.lua
@@ -172,6 +172,15 @@ return {
         end, opts.ensure_installed)
       end
       require("nvim-treesitter.configs").setup(opts)
+      require("nvim-treesitter.parsers").get_parser_configs().just = {
+        install_info = {
+          url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
+          files = { "src/parser.c", "src/scanner.c" },
+          branch = "main",
+          -- use_makefile = true -- this may be necessary on MacOS (try if you see compiler errors)
+        },
+        maintainers = { "@IndianBoy42" },
+      }
     end,
   },
   {
