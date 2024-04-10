81ba569696e3e677076e87c02fd1525c095d02b1
Yofriadi Yahya <yofriadi.yahya@pinhome.id>
2024-04-10 19:24:25 +0700

    configure fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,61 @@
 # ~>
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    atuin init fish | source
+    eval (zellij setup --generate-auto-start fish | string collect)
 end
 
-starship init fish | source
+fish_vi_key_bindings
+
+# Neovim
+set -Ux PATH $HOME/.local/share/bob/nvim-bin $PATH
+
+# Golang
+set -Ux GOMODCACHE $HOME/.cache/go
+set -Ux GOBIN $GOPATH/bin
+set -Ux PATH $GOBIN $PATH
+
+# PNPM
+set -Ux PNPM_HOME /home/ypcl/.local/share/pnpm
+if not string match -q "*:$PNPM_HOME:*" $PATH
+    set -Ux PATH $PNPM_HOME $PATH
+end
+
+# Gcloud
+if test -f ~/.local/bin/google-cloud-sdk/path.fish.inc
+    source ~/.local/bin/google-cloud-sdk/path.fish.inc
+end
+
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+alias n='NVIM_APPNAME=nvim/default nvim'
+alias na='NVIM_APPNAME=nvim/AstroNvim nvim'
+
+alias l="eza"
+alias ll="eza -l"
+alias la="eza -a"
+alias lla="eza -la"
+
+alias zl="zellij"
+alias hx="helix"
+
+alias cat="bat"
+set -Ux BAT_THEME tokyonight_storm
+
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign --message "--wip-- [skip ci]"'
+alias gunwip='git rev-list --max-count=1 --format="%s" HEAD | grep -q "\--wip--" && git reset HEAD~1'
+
+# Similar to `gunwip` but recursive "Unwips" all recent `--wip--` commits not just the last one
+function gunwipall
+    set -l _commit (git log --grep='--wip--' --invert-grep --max-count=1 --format=format:%H)
+
+    # Check if a commit without "--wip--" was found and it's not the same as HEAD
+    if test "$_commit" != (git rev-parse HEAD)
+        git reset $_commit; or return 1
+    end
+end
 
 function nvim_paste
     # Check for win32yank.exe executable
@@ -15,3 +67,6 @@ function nvim_paste
         powershell.exe Get-Clipboard | tr -d '\r' | sed -z '$ s/\n$//'
     end
 end
+
+zoxide init fish | source
+starship init fish | source
