ce9792f5f71bbde5a6a6b2855dd98f6475ea1337
Yofriadi Yahya <yofriadi.yahya@pinhome.id>
2024-06-04 20:28:26 +0700

    fixed noice.nvim warning

--- .config/nvim/AstroNvim/lua/editing/lsp.lua
+++ .config/nvim/AstroNvim/lua/editing/lsp.lua
@@ -174,14 +174,23 @@ return {
     ft = "lua", -- only load on lua files
     opts = {
       library = {
-        vim.env.LAZY .. "/luvit-meta/library", -- see below
-        -- You can also add plugins you always want to have loaded.
-        -- Useful if the plugin has globals or types you want to use
-        -- vim.env.LAZY .. "/LazyVim", -- see below
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
       },
     },
   },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings for lazydev
+  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+  { -- optional completion source for require statements and module annotations
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
+  },
   { "dmmulroy/ts-error-translator.nvim" },
   { "brenoprata10/nvim-highlight-colors", opts = {} },
 }
--- .config/nvim/AstroNvim/lua/editor/starter.lua
+++ .config/nvim/AstroNvim/lua/editor/starter.lua
@@ -106,4 +106,69 @@ return {
       return opts
     end,
   },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = function(_, opts)
+      local utils = require "astrocore"
+      return utils.extend_tbl(opts, {
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+        notify = {
+          enabled = false,
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(
+          opts.ensure_installed,
+          { "bash", "markdown", "markdown_inline", "regex", "vim" }
+        )
+      end
+    end,
+  },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@param opts AstroLSPOpts
+    opts = function(_, opts)
+      local noice_opts = require("astrocore").plugin_opts "noice.nvim"
+      -- disable the necessary handlers in AstroLSP
+      if not opts.lsp_handlers then opts.lsp_handlers = {} end
+      if vim.tbl_get(noice_opts, "lsp", "hover", "enabled") ~= false then
+        opts.lsp_handlers["textDocument/hover"] = false
+      end
+      if vim.tbl_get(noice_opts, "lsp", "signature", "enabled") ~= false then
+        opts.lsp_handlers["textDocument/signatureHelp"] = false
+      end
+    end,
+  },
+  {
+    "heirline.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local noice_opts = require("astrocore").plugin_opts "noice.nvim"
+      if vim.tbl_get(noice_opts, "lsp", "progress", "enabled") ~= false then -- check if lsp progress is enabled
+        opts.statusline[9] = require("astroui.status").component.lsp { lsp_progress = false }
+      end
+    end,
+  },
 }
