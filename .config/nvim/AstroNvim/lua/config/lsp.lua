1c352cb3759c354a0ded47667acb6e579b0adbd1
Yofriadi Yahya <yofriadi.yahya@pinhome.id>
2024-07-08 06:40:22 +0700

    update AstroNvim

--- .config/nvim/AstroNvim/lua/config/lsp.lua
+++ .config/nvim/AstroNvim/lua/config/lsp.lua
@@ -15,6 +15,15 @@ return {
           virtual_text = false,
           underline = true,
         },
+        -- Configure core features of AstroNvim
+        features = {
+          large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+          autopairs = true, -- enable autopairs at start
+          cmp = true, -- enable completion at start
+          diagnostics_mode = 1, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+          highlighturl = true, -- highlight URLs at start
+          notifications = true, -- enable notifications at start
+        },
       })
     end,
   },
